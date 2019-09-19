d5521ddd6021c4df8900ab70c4de50bcd8527aab
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-09-19 16:33:11 -0300

    Funcoes com parametros no Playground

---
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var name = "Catia"
+var project = "Eggplant brownie"
+
+name = "Kaka"
+print(name)
+print (project)
+
+func helloCalorias(){
+    let product = "brownie"
+    print("Hello Calorias! \(product) ")
+}
+
+helloCalorias()  // chamando a funcao
+
+var estado = "😆"
+print(estado)
+
+var 😡 = "kakak"
+print(😡)
+
+func add(name:String, calories:Double){ // funacao com parametro
+    print("adding \(name) with \(calories)")
+}
+add(name: "Brownie", calories: 30.5)
