2c485d46f747023a106675583ae748cac2121a7d
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-09-20 09:51:34 -0300

    Arrays

--- eggplant-brownie/MyPlayground.playground/Contents.swift
+++ eggplant-brownie/MyPlayground.playground/Contents.swift
@@ -20,7 +20,23 @@ print(estado)
 var 😡 = "kakak"
 print(😡)
 
-func add(name:String, calories:Double){ // funacao com parametro
+func add(name:String, calories:Double){ // funcao com parametro
     print("adding \(name) with \(calories)")
 }
 add(name: "Brownie", calories: 30.5)
+
+// manipulando arrays
+let calorias = [50.5, 60, 10.7, 500]
+print("\(calorias ) imprimindo o array inteiro")
+
+for i in 0...3{
+    print("\(calorias[i] ) imprimindo a posicao do array")}
+
+for i in 0...(calorias.count - 1){
+    print(calorias[i])
+}
+print()
+for call in calorias{
+    print(call)
+}
+
