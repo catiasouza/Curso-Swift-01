b45711c1811c66cd0bef02e4bcf56ea06282dd72
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-09-20 16:35:18 -0300

    Organizando os arquivos no projeto

---
+++ eggplant-brownie/models/Item.swift
@@ -0,0 +1,14 @@
+
+import Foundation
+
+
+class Item {
+    
+    var name: String
+    var calories: Double
+    
+    init(name:String, calories: Double) {
+        self.name = name
+        self.calories = calories
+    }
+}
