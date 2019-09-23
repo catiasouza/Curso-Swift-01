724a0e5937d07c94d354d82ecfca9e076963ea0a
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-09-23 09:46:38 -0300

    Correcoes na viewController

--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -10,15 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var nameField : UITextField!
-    @IBOutlet var happinessField : UITextField!
+    @IBOutlet var nameField : UITextField?
+    @IBOutlet var  happinessField: UITextField?
 
     @IBAction func add(){
+        if(nameField == nil || happinessField == nil){
+            return
+        }
+        let name:String = nameField!.text!
         
-        let name = nameField.text
-        let happiness = happinessField.text
+        if let happiness = Int(happinessField!.text!){
+            let meal = Meal(name: name, happiness: happiness)
+            print("eaten \(meal.name) with happiness \(meal.happiness)!")
+        }
         
-        print("Eaten \(name) with happines \(happiness)!")
     }
 
 }
