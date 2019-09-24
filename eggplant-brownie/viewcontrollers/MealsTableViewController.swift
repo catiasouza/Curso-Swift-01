c40e52708f98a6e5ffd9fc0c01f2aa2b8119b4ce
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-09-24 11:08:57 -0300

    Adicionando elemento na lista

--- eggplant-brownie/viewcontrollers/MealsTableViewController.swift
+++ eggplant-brownie/viewcontrollers/MealsTableViewController.swift
@@ -2,17 +2,27 @@
 
 import UIKit
 
-class MealsTableViewController : UITableViewController{
+class MealsTableViewController : UITableViewController {
     
     var meals:Array<Meal> = [Meal(name: "Eggplante", happiness: 5),
                              Meal(name: "Zuchini Muffin", happiness: 4),
                              Meal(name: "Hamburguer", happiness: 10),
                              Meal(name: "Bolo", happiness: 8)]
     
+    //0 _ vc evita chamar o nome do parametro
+    func add(_ meal: Meal){           //funcao para adicionar elemento na lista
+        meals.append(meal)          //refeicao adiciona refeicao
+        tableView.reloadData()      //TableView recarregue os dados
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let view  = segue.destination as! ViewController
+        view.mealsTable = self
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return meals.count
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let row = indexPath.row
         let meal = meals[row]
         let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: nil)
--- eggplant-brownie/viewcontrollers/ViewController.swift
+++ eggplant-brownie/viewcontrollers/ViewController.swift
@@ -1,35 +1,30 @@
-//
-//  ViewController.swift
-//  eggplant-brownie
-//
-//  Created by Catia Miranda de Souza on 19/09/19.
-//  Copyright © 2019 Catia Miranda de Souza. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
-    
-    @IBOutlet var nameField : UITextField?
-    @IBOutlet var  happinessField: UITextField?
 
+    @IBOutlet var nameField: UITextField?
+    @IBOutlet var happinessField: UITextField?
+    var mealsTable: MealsTableViewController?
+    
     @IBAction func add(){
+        
         if(nameField == nil || happinessField == nil){
             return
         }
+        
         let name:String = nameField!.text!
         
         if let happiness = Int(happinessField!.text!){
             let meal = Meal(name: name, happiness: happiness)
-            print("eaten \(meal.name) with happiness \(meal.happiness)!")
-            
-            if let navigation =  navigationController {
-                
-                navigationController!.popViewController(animated: true)
+            print("eaten \(meal.name) with happines \(meal.happiness)!")
+            if (mealsTable == nil){
+                return
+            }
+            mealsTable?.add(meal)    //chamando o add refeicao do mealsTable
+            if let navigation = navigationController{
+                navigation.popViewController(animated: true)
+            }
         }
-        
     }
-
-}
-
 }
